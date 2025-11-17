f6eede1049f98dd8b4d31cdf9aa56f487b51b84f
Adrian Richter <adrian@intevation.de>
2025-11-17 16:30:21 +0100

    Make port and gunicorn worker timeout settable
    ... trough env vars.

--- dev/command.sh
+++ dev/command.sh
@@ -2,4 +2,4 @@
 
 if [ "$APP_CONTEXT" = "dev"   ]; then exec flask --app src/mediaserver run --host 0.0.0.0 --port 9006 --debug; fi
 if [ "$APP_CONTEXT" = "tests" ]; then sleep inf; fi
-if [ "$APP_CONTEXT" = "prod"  ]; then exec gunicorn -b 0.0.0.0:9006 src.mediaserver:app; fi
+if [ "$APP_CONTEXT" = "prod"  ]; then exec gunicorn -b "0.0.0.0:$MEDIA_PORT" -t "${OPENSLIDES_MEDIA_WORKER_TIMEOUT:-30}" src.mediaserver:app; fi
